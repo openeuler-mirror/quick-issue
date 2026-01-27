343205e7e5440467ccb8e70fbc9f92a27a7ffe8c
zjwmiao <1723168479@qq.com>
2026-01-27 19:27:03 +0800

    feat: 引入opendesign-plus 埋点、CookieNotice相关组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { watch, onMounted } from 'vue';
+import { watch, onMounted, ref, nextTick } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useLangStore } from '@/stores';
 
@@ -10,8 +10,10 @@ import AppHeader from '@/components/AppHeader.vue';
 import AppFooter from '@/components/AppFooter.vue';
 
 import { refreshInfo } from '@/shared/login';
-import CookieNotice from './components/CookieNotice.vue';
 import FloatingButton from '@/components/FloatingButton.vue';
+import { OPlusConfigProvider } from '@opendesign-plus/components';
+import { OElCookieNotice } from '@opendesign-plus/components/element-plus';
+import { useRoute } from 'vue-router';
 
 refreshInfo();
 const { locale } = useI18n();
@@ -28,12 +30,26 @@ watch(
     locale.value = val;
   }
 );
+
+const cookieNoticeVisible = ref(false);
+const MAIN_URL = import.meta.env.VITE_MAIN_DOMAIN_URL;
+const cookieRef = ref();
+const route = useRoute();
+watch(
+  () => route.path,
+  async () => {
+    await nextTick();
+    cookieRef.value?.check();
+  }
+);
 </script>
 
 <template>
   <header><AppHeader /></header>
   <main><RouterView></RouterView></main>
-  <CookieNotice />
+  <OPlusConfigProvider :locale="langStore.lang">
+    <OElCookieNotice ref="cookieRef" v-model:visible="cookieNoticeVisible" community="openEuler" :detail-url="`${MAIN_URL}/${langStore.lang}/other/cookies/`" />
+  </OPlusConfigProvider>
   <footer><AppFooter /></footer>
   <FloatingButton />
 </template>
@@ -47,6 +63,22 @@ watch(
   background-size: 100% auto;
   background-repeat: repeat;
   background-color: rgb(1, 10, 51);
+  --layout-content-max-width: 1544px;
+  --layout-content-padding: 64px;
+  --layout-header-height: 80px;
+  
+  @include respond-to('<=laptop') {
+    --layout-content-max-width: 100%;
+    --layout-content-padding: 40px;
+  }
+  
+  @include respond-to('<=pad') {
+    --layout-content-padding: 32px;
+  }
+  
+  @include respond-to('phone') {
+    --layout-content-padding: 24px;
+  }
 }
 
 main {
